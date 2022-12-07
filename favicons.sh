56ec8ca32ffd96bd90f5e9dc4e1d153834ad0db5
AbdoArafh <abdoarafh54@gmail.com>
2022-12-07 23:51:53 +0200

    added: pwa

---
+++ favicons.sh
@@ -0,0 +1,4 @@
+cd public
+inkscape favicon.svg -w 180 -h 180 -o apple-touch-icon.png
+inkscape favicon.svg -w 192 -h 192 -o 192x192.png
+inkscape favicon.svg -w 512 -h 512 -o 512x512.png
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  base: "/calculator/",
+  plugins: [
+    svelte(),
+    VitePWA({
+      includeAssets: ["favicon.svg", "apple-touch-icon.png"],
+      manifest: {
+        name: "Calculator",
+        short_name: "Calculator",
+        description: "A simple calculator app",
+        theme_color: "#001E34",
+        icons: [
+          {
+            src: "192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
