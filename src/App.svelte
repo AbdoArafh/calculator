50d38d38342a3b47f335d4d67ca306313143f591
AbdoArafh <abdoarafh54@gamil.com>
2023-01-09 07:56:47 +0200

    added: some nice colors

--- src/App.svelte
+++ src/App.svelte
@@ -8,16 +8,16 @@
   // lib
   import Button from "./lib/Button";
 
-  type ArthemticOperation = "+" | "-" | "*" | "/"
+  type ArthemticOperation = "+" | "-" | "*" | "/";
 
   let num = "";
   let prev_num = "";
-  let reset_flag = false
-  let latest_operation = "+"
+  let reset_flag = false;
+  let latest_operation = "+";
 
   const stringToNumber = (n: string) => {
-    return Number(eval(n.replace("%", "/100")))
-  }
+    return Number(eval(n.replace("%", "/100")));
+  };
 
   const performOperation = (operation?: ArthemticOperation) => {
     if (operation) latest_operation = operation;
@@ -25,16 +25,22 @@
       prev_num = num;
       num = "";
       reset_flag = false;
-      return
+      return;
     }
     if (prev_num) {
-      num = window.eval(`${stringToNumber(prev_num)} ${latest_operation} ${stringToNumber(num)}`).toString()
-      reset_flag = true
-      return
+      num = window
+        .eval(
+          `${stringToNumber(prev_num)} ${latest_operation} ${stringToNumber(
+            num
+          )}`
+        )
+        .toString();
+      reset_flag = true;
+      return;
     }
-    prev_num = num.slice(0)
-    num = ""
-  }
+    prev_num = num.slice(0);
+    num = "";
+  };
 
   const addChar = (char: string) => {
     if (reset_flag) {
@@ -43,41 +49,44 @@
       prev_num = "";
       return;
     }
-    if (num.search(/%/) !== -1) return; 
-    num += char
-  }
+    if (num.search(/%/) !== -1) return;
+    num += char;
+  };
 
   const addNumber = (char: string) => {
     return () => {
       addChar(char);
-    }
-  }
+    };
+  };
 
   const addDecimalPoint = () => {
     if (num.search(/\./) !== -1) return;
-    addChar(".")
-  }
+    addChar(".");
+  };
 
   const clear = () => {
-    num = ""
-  }
+    num = "";
+  };
 
   const backspace = () => {
-    num = num.slice(0, num.length - 1)
-  }
+    num = num.slice(0, num.length - 1);
+  };
 
   const addPercent = () => {
     if (num.search(/%/) !== -1) return;
-    addChar("%")
-  }
+    addChar("%");
+  };
 
   const createNumberRow = (row: string[]): Button[] => {
-    return row.map((n) => new Button({
-      type: "number",
-      symbol: n,
-      callback: addNumber(n)
-    }))
-  }
+    return row.map(
+      (n) =>
+        new Button({
+          type: "number",
+          symbol: n,
+          callback: addNumber(n),
+        })
+    );
+  };
 
   const buttons = [
     new Button({
@@ -110,14 +119,14 @@
     new Button({
       type: "operation",
       symbol: "+",
-      callback: () => performOperation("+")
+      callback: () => performOperation("+"),
     }),
     ...createNumberRow(["1", "2", "3"]),
     new Button({
       type: "operation",
       symbol: "=",
       callback: () => performOperation(),
-      elongated: true
+      elongated: true,
     }),
     new Button({
       type: "operation",
@@ -138,9 +147,11 @@
 
   onMount(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
-      Array.from(Array(10)).map((_, i) => i.toString()).forEach(i => {
-        if (event.key === i) addNumber(i)();
-      });
+      Array.from(Array(10))
+        .map((_, i) => i.toString())
+        .forEach((i) => {
+          if (event.key === i) addNumber(i)();
+        });
 
       if (event.key === ".") addDecimalPoint();
     };
@@ -153,7 +164,7 @@
       if (event.key === "Enter") performOperation();
       if (event.key === "Backspace") backspace();
       if (event.key.toLowerCase() === "c") clear();
-    }
+    };
 
     window.addEventListener("keypress", handleKeyPress);
     window.addEventListener("keyup", handleKeyUp);
@@ -170,9 +181,12 @@
     <span>{num}</span>
   </div>
 
-  <div class="body"> 
+  <div class="body">
     {#each buttons as button}
-      <button class={classNames("button", {elongated: button.elongated})} on:click={button.callback}>{button.symbol}</button>
+      <button
+        class={classNames("button", { elongated: button.elongated })}
+        on:click={button.callback}>{button.symbol}</button
+      >
     {/each}
   </div>
 </main>
@@ -183,7 +197,7 @@
     /* aspect-ratio: 9 / 16; */
     max-width: 640px;
     width: 100vw;
-    background-color: #001E34;
+    background-color: #001e34;
 
     display: flex;
     flex-direction: column;
@@ -218,7 +232,7 @@
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: #002B4A;
+    background-color: #002b4a;
     color: #ffffff;
     cursor: pointer;
     user-select: none;
@@ -237,6 +251,12 @@
 
   .button.elongated {
     grid-row: span 2;
-    background-color: #FFA800;
+    background-color: #ffa800;
+  }
+  .button {
+    background-color: rgb(0, 229, 255);
+    animation: alternate;
+    /* border: 3px solid rgba(255, 255, 255, 0.5); */
+    box-shadow: inset 0 0 5px 5px white;
   }
 </style>
